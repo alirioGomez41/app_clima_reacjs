f88f1f1026b8398c88c84d1b992642204aa89a4e
aliriogomez41 <alirio.fernandez24@gmail.com>
2020-11-28 23:02:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+import Clima from './components/Clima'
+import Error from './components/Error';
+
 
 function App() {
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad:'',
+    pais:''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+  const {ciudad, pais} = busqueda;
+
+
+
+  useEffect(() => {
+   
+    if(consultar)
+    {
+      consultaAPI();
+      guardarConsultar(false);
+
+    }
+    //eslint-disable-next-line
+  }, [consultar]);
+
+const consultaAPI = async ()=> {
+  const KEY = '56d92caae0ee42bb2d070841d2e8506b'; 
+  const API = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${KEY}`;
+
+  const respuesta = await fetch(API);
+  const resultado = await respuesta.json();
+  if(resultado.cod === "404"){
+    guardarError(true);
+  }else{
+    guardarError(false);
+  }
+  guardarResultado(resultado);
+} 
+
+
+if(error){
+  
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        titulo="Clima React App"
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+             {
+               error?
+                (
+                  <Error 
+                    mensaje="No hay resultados"
+                  />
+                )
+               :
+               (
+                <Clima
+                  resultado={resultado}
+                />
+               )
+
+             }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
